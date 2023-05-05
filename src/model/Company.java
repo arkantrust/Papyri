9386a8a9dc04150808dc4bc1d67394bda57c8ec1
Arkantrust <davidulce26@gmail.com>
2023-05-05 08:08:53 -0500

    Created Company Class

--- src/model/Company.java
+++ src/model/Company.java
@@ -1,14 +1,23 @@
 package model;
+import java.util.ArrayList;
 
 public class Company {
+    //attributes
     private String name;
     private String nit;
     private String address;
+
+    //constants
+    public static final double PAYMONTH = 5; // USD
+
+    //relations
+    private ArrayList<User> users;
 
     public Company(String name, String nit, String address) {
         this.name = name;
         this.nit = nit;
         this.address = address;
+        users = new ArrayList<>();
     }
 
     public String getName() {
@@ -28,5 +37,13 @@ public class Company {
     }
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
     }
 }
