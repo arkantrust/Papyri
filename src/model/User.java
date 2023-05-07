6e847fa83c1ac7fe9837c9961e8af7d793f98514
Arkantrust <davidulce26@gmail.com>
2023-05-06 22:56:56 -0500

    Added methods

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,5 @@
 package model;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public abstract class User {
@@ -7,9 +8,10 @@ public abstract class User {
     protected String id;
     protected String email;
     protected Calendar initDate; // Date the user signed up
-    
-    public User(String name, String id, Calendar initDate) {
+
+    public User(String name, String email, String id, Calendar initDate) {
         this.name = name;
+        this.email = email;
         this.id = id;
         this.initDate = initDate;
     }
@@ -36,6 +38,22 @@ public abstract class User {
     }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String DateToString(Calendar date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String dateToString = dateFormat.format(date.getTime());
+        return dateToString;
+    }
+
+    @Override
+    public String toString() {
+        String info = "";
+        info += "Name: " + name + "\n";
+        info += "Email: " + email + "\n";
+        info += "ID: " + id + "\n";
+        info += "Registered: " + DateToString(initDate) + "\n";
+        return info;
     }
 
     public abstract String surprise(int month, char randLetter);
