9c8044caebb7bc17c377889d1b0b58df3fd23442
David <davidulce26@gmail.com>
2023-05-12 08:42:27 -0500

    Created Product class

---
+++ src/model/Product.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.Calendar;
+
+public abstract class Product {
+    String id;
+    String name;
+    Calendar publicationDate;
+    double cost;
+    
+    public Product(String id, String name, Calendar publicationDate, double cost) {
+        this.id = id;
+        this.name = name;
+        this.publicationDate = publicationDate;
+        this.cost = cost;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Calendar getPublicationDate() {
+        return publicationDate;
+    }
+    public void setPublicationDate(Calendar publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+    public double getCost() {
+        return cost;
+    }
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    
+}
+
