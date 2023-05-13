b8b3fbc14a558f46801188ef2529efcf028b280e
Arkantrust <davidulce26@gmail.com>
2023-05-13 15:22:20 -0500

    Added getters and setters

--- src/model/Book.java
+++ src/model/Book.java
@@ -8,14 +8,43 @@ public class Book extends BibliographicProduct {
     private int copiesSold;
     private int readPages;
 
-
     public Book(String id, String name, Calendar publicationDate, int pages, String coverURL, double cost, String review, int genre, int copiesSold, int readPages) {
         super(id, name, publicationDate, pages, coverURL, cost);
         this.review = review;
-        this.genre = BookGenre.getType(genre); // TODO: create getType switch method in BookGenre 1, 2, 3 respectively
+        this.genre = BookGenre.getGenre(genre); // TODO: create getType switch method in BookGenre 1, 2, 3 respectively
         this.copiesSold = copiesSold;
         this.readPages = readPages;
     }
 
-    
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public BookGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(BookGenre genre) {
+        this.genre = genre;
+    }
+
+    public int getCopiesSold() {
+        return copiesSold;
+    }
+
+    public void setCopiesSold(int copiesSold) {
+        this.copiesSold = copiesSold;
+    }
+
+    public int getReadPages() {
+        return readPages;
+    }
+
+    public void setReadPages(int readPages) {
+        this.readPages = readPages;
+    }
 }
