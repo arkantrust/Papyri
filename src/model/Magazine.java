d53095d2ed2fc0c53d311732f81282f9f472a938
Arkantrust <davidulce26@gmail.com>
2023-05-13 18:31:48 -0500

    Added getters and setters for Magazine class

--- src/model/Magazine.java
+++ src/model/Magazine.java
@@ -1,9 +1,10 @@
 package model;
+
 import java.util.Calendar;
 
-public class Magazine extends BibliographicProduct{
-    private MagazineCategory category; //TODO: Create MagazineCategory enum {Variedades, Diseño y Científica}
-    private IssuanceFrequency freq; //TODO: Create enum {yearly, monthly, weekly, daily}
+public class Magazine extends BibliographicProduct {
+    private MagazineCategory category; // TODO: Create MagazineCategory enum {Variedades, Diseño y Científica}
+    private IssuanceFrequency freq; // TODO: Create enum {yearly, monthly, weekly, daily}
     private int subs;
 
     public Magazine(String id, String name, Calendar publicationDate, int pages, String coverURL,
@@ -13,3 +14,28 @@ public class Magazine extends BibliographicProduct{
         this.freq = IssuanceFrequency.get(freq);
         this.subs = subs;
     }
+
+    public MagazineCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(MagazineCategory category) {
+        this.category = category;
+    }
+
+    public IssuanceFrequency getFreq() {
+        return freq;
+    }
+
+    public void setFreq(IssuanceFrequency freq) {
+        this.freq = freq;
+    }
+
+    public int getSubs() {
+        return subs;
+    }
+
+    public void setSubs(int subs) {
+        this.subs = subs;
+    }
+}
