fbb9fd1a0cc893b1db0fdacea2dbf50b578e2e69
Arkantrust <davidulce26@gmail.com>
2023-05-13 15:22:37 -0500

    Created BookGenre enum

---
+++ src/model/BookGenre.java
@@ -0,0 +1,24 @@
+package model;
+
+public enum BookGenre {
+    SCIENCE_FICTION,
+    FANTASY,
+    HISTORICAL_NOVEL;
+
+    public static BookGenre getGenre(int intGenre) {
+        BookGenre genre = null;
+
+        switch (intGenre) {
+            case 1:
+                genre = SCIENCE_FICTION;
+                break; 
+            case 2:
+                genre = FANTASY;
+                break;
+            case 3:
+                genre = HISTORICAL_NOVEL;
+                break;
+        }
+        return genre;
+    }
+}
