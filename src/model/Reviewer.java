295b12b4e326ed760252ea6c0b4588c063bf2b92
Arkantrust <davidulce26@gmail.com>
2023-05-19 03:53:17 -0500

    Implemented surprise

--- src/model/Reviewer.java
+++ src/model/Reviewer.java
@@ -3,23 +3,23 @@ package model;
 import java.util.Calendar;
 
 public class Reviewer extends PremiumUser {
-    private Interest interest; // TODO: Create enum Interest
+    private String interest; // TODO: Create enum Interest
     private int reviewCount;
     private String blog;
 
     public Reviewer(String name, String email, String password, String id, int internalID, Calendar initDate,
-            String nickname, String avatar, String card, int lastPaidMonth, double[] payments,  Interest interest, int reviewCount, String blog) {
+            String nickname, String avatar, String card, int lastPaidMonth, double[] payments,  String interest, int reviewCount, String blog) {
         super(name, email, password, id, internalID, initDate, nickname, avatar, card, lastPaidMonth, payments);
         this.interest = interest;
         this.reviewCount = reviewCount;
         this.blog = blog;
     }
 
-    public Interest getInterest() {
+    public String getInterest() {
         return interest;
     }
 
-    public void setInterest(Interest interest) {
+    public void setInterest(String interest) {
         this.interest = interest;
     }
 
@@ -50,5 +50,13 @@ public class Reviewer extends PremiumUser {
     }
     
     @Override
-    public surprise(internalID, 0) {} // TODO: Implement surprise method
+    public String surprise(int randInt, char s) {
+        var message = "";
+        if (randInt < reviewCount) {
+            message += "Congratulations! You won a trip to San Andres!\n";
+        } else {
+            message = "Sorry, you didn't get a surprise.\n";
+        }
+        return message;
+    }
 }
