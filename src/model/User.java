dd910a255468f7b6cd2f9f427d29f2979628c36f
Arkantrust <davidulce26@gmail.com>
2023-05-25 18:02:27 -0500

    Changed User attributes

--- src/model/User.java
+++ src/model/User.java
@@ -1,33 +1,24 @@
 package model;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
 public abstract class User implements DateUsage {
 
     protected String name;
-    protected int internalID;
     protected String email;
-    protected Calendar initDate; // Date the user signed up
-    protected boolean ads;
-    protected ArrayList<Product> productsOwned;
-    protected int productsOwnedCount;
-    protected String productsOwnedList;
-    protected String password;
     protected String id;
+    protected String password;
+    protected Calendar initDate; // Date the user signed up
+    protected int internalID;
 
 
-    public User(String name, String email, String password, String id, int internalID, Calendar initDate, boolean ads) {
+    public User(String name, String email, String password, String id, int internalID, Calendar initDate) {
         this.name = name;
         this.email = email;
-        this.password = password;
         this.id = id;
-        this.internalID = internalID;
+        this.password = password;
         this.initDate = initDate;
-        this.ads = ads;
-        productsOwned = new ArrayList<>();
-        this.productsOwnedCount = 0;
-        this.productsOwnedList = "";
+        this.internalID = internalID;
     }
 
     public String getName() {
@@ -38,68 +29,12 @@ public abstract class User implements DateUsage {
         this.name = name;
     }
 
-    public int getInternalID() {
-        return internalID;
-    }
-
-    public void setInternalID(int internalID) {
-        this.internalID = internalID;
-    }
-
-    public Calendar getInitDate() {
-        return initDate;
-    }
-
-    public void setInitDate(Calendar initDate) {
-        this.initDate = initDate;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public boolean isAds() {
-        return ads;
-    }
-
-    public void setAds(boolean ads) {
-        this.ads = ads;
-    }
-
-    public ArrayList<Product> getProductsOwned() {
-        return productsOwned;
-    }
-
-    public void setProductsOwned(ArrayList<Product> productsOwned) {
-        this.productsOwned = productsOwned;
-    }
-
-    public int getProductsOwnedCount() {
-        return productsOwnedCount;
-    }
-
-    public void setProductsOwnedCount(int productsOwnedCount) {
-        this.productsOwnedCount = productsOwnedCount;
-    }
-
-    public String getProductsOwnedList() {
-        return productsOwnedList;
-    }
-
-    public void setProductsOwnedList(String productsOwnedList) {
-        this.productsOwnedList = productsOwnedList;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getID() {
@@ -110,10 +45,28 @@ public abstract class User implements DateUsage {
         this.id = id;
     }
 
-    public void addProduct(Product bought) {
-        productsOwned.add(bought);
-        productsOwnedList += bought.toString();
-        productsOwnedCount++;
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Calendar getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(Calendar initDate) {
+        this.initDate = initDate;
+    }
+
+    public int getInternalID() {
+        return internalID;
+    }
+
+    public void setInternalID(int internalID) {
+        this.internalID = internalID;
     }
 
     @Override
@@ -125,6 +78,4 @@ public abstract class User implements DateUsage {
         info += "Registered: " + DateUsage.DateToString(initDate) + "\n";
         return info;
     }
-
-    public abstract String surprise(int month, char randLetter);
 }
